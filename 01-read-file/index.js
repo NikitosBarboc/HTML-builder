458a32ad567d0b3ace9f03f74d94f234641fba7a
Nik-portfolio <kravtcovmikita@gmail.com>
2022-05-21 20:07:20 +0300

    feat: solve 01-read-file (Sat, May 21, 2022, 20:00:40)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
-const output = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
-output.on('data', chunk => console.log(chunk));
+const result = fs.createReadStream('./text.txt', 'utf-8');
+result.on('data', chunk => console.log(chunk));
+
+
+
 
